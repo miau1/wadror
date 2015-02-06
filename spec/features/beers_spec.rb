4de3faafdbc8906a6519e50d0f177f80326e2b56
miau1 <aulamo.mikko@gmail.com>
2015-02-06 10:40:13 +0200

    viikko 4

--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 describe "Beer" do
+  let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
+  let!(:user) { FactoryGirl.create :user }
+
+  before :each do
+    sign_in(username:"Pekka", password:"Foobar1")
+  end
+
   it "is added when given a valid name" do
     visit new_beer_path
-
+    save_and_open_page
     fill_in('beer_name', with:'olut')
     expect{
       click_button('Create Beer')
